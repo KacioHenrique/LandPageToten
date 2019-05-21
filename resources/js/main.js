3b6e51a3f0b379750afdb10eef69ccf071c90b6a
Caio Gomes <caiogomes34@gmail.com>
2019-05-21 14:23:04 -0300

    fix: correct instagram link

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,6 +1,6 @@
 function openInstagram() {
 	window.open(
-	  'https:https://instagram.com/totemclube/',
+	  'https://instagram.com/totemclube/',
 	  '_blank' // <- This is what makes it open in a new window.
 	);
 }
